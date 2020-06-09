88e0f3ab58b3db3046d1967b97106cc96348e67f
Francisco Veloz <franciscoveloz245@gmail.com>
2020-06-08 19:34:00 -0500

    Agregamos al proyecto la estructura inicial de los sockets y de johnny-five... falta de agregar el socket.io a las vistas y el js

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ const entRoute = require('./routes/entrada')
 const salaRoute = require('./routes/sala')
 const jardinRoute = require('./routes/jardin')
 const banoRoute = require('./routes/bano')
+const http = require('http')
+const five = require("johnny-five")
+const socketIO = require("socket.io")
+const WebSocket = require("ws");
 
 var app = express()
+const servidor = http.createServer(app)
+const circuito = new five.Board()
+const io = socketIO.listen(servidor)
+const wSocket = new WebSocket.Server({port: 8080});
+
+//iniciar Servidor
+servidor.listen(5000, () => {
+    console.log('Servidor corriendo en el puerto 5000')
+})
 
 //archivos estaticos 
 app.use('/', express.static('public'))
@@ -26,8 +39,42 @@ app.use('/sala', salaRoute)
 app.use('/jardin', jardinRoute)
 app.use('/bano', banoRoute)
 
-//iniciar Servidor
-app.listen(5000, () => {
-    console.log('Servidor corriendo en el puerto 5000')
+
+//Todo lo relacionado a Johnny-five
+
+var hola = "Hola"
+
+circuito.on("ready", () => {
+
+    wSocket.on("connection", (ws, req) => {
+        
+        console.log("ws conectado...")
+
+        ws.on("message", datos => {
+            
+            switch(datos)
+            {
+                case 0:
+                    console.log("Ws hola mundo")
+                    break;
+            }
+
+        })
+    })
+
+    ImprimirSensores()
 })
+
+function ImprimirSensores()
+{
+    Prueba()
+
+    setTimeout(ImprimirSensores, "1000")
+}
+
+function Prueba()
+{
+    io.emit("Hola", hola)
+}
+
 
